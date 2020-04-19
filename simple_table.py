2ef0a8547bc105a7f377b091a2768bdb238ff205
johntelforduk <jtelford1@gmail.com>
2020-04-19 13:47:20 +0100

    Encode using JSON.

--- simple_table.py
+++ simple_table.py
@@ -1,6 +1,7 @@
 # Simple, durable tables.
 
 from os import path
+import json
 
 
 class SimpleTable:
@@ -8,31 +9,22 @@ class SimpleTable:
     def __init__(self, filename: str):
         """Create a table that will be made durable by writing to parm filename."""
 
-        self.filename = filename
-        self.new_rows = []          # List of rows added to the table in this session.
-        self.rows = []              # Union of old and new rows, ie. all of the rows in the table.
+        self.filename = filename            # JSON file that the table will be persisted to.
+        self.rows = []                      # List of rows in the table.
 
         # Read any previous contents of the file into memory.
         if path.exists(self.filename):
             f = open(self.filename, 'r')
             whole_text = (f.read())
-            for each_line in whole_text.split():
-                self.rows.append(each_line)
+            self.rows = json.loads(whole_text)
             f.close()
 
-    def insert(self, row: str):
+    def insert(self, row: list):
         """Insert the parm row into the table."""
-        self.new_rows.append(row)
         self.rows.append(row)
 
     def commit(self):
         """Write the table to durable storage."""
-        if path.exists(self.filename):
-            f = open(self.filename, 'a')
-        else:
-            f = open(self.filename, 'w')
-
-        while len(self.new_rows) > 0:
-            this_addition = self.new_rows.pop(0)
-            f.write(str(this_addition) + '\n')
+        f = open(self.filename, 'w')
+        f.writelines(json.dumps(self.rows))
         f.close()
--- test_simple_table.py
+++ test_simple_table.py
@@ -2,14 +2,14 @@
 
 from simple_table import SimpleTable
 from os import path, remove
-
 import unittest
 
 
 class TestSimpleTable(unittest.TestCase):
 
-    def test_insert(self):
-        filename = 'table_scratch.txt'
+    def test_insert_and_commit(self):
+        """Test row inserts and saving to file for simple, single column rows."""
+        filename = 'table_scratch_simple.json'
 
         # At start of test, ensure the file doesn't exist.
         if path.exists(filename):
@@ -18,29 +18,49 @@ class TestSimpleTable(unittest.TestCase):
         first_table = SimpleTable(filename)
         self.assertEqual(first_table.rows, [])
 
-        first_table.insert('Tom')
-        self.assertEqual(first_table.rows, ['Tom'])
+        first_table.insert(['Tom'])
+        self.assertEqual(first_table.rows, [['Tom']])
 
-        first_table.insert('Dick')
-        first_table.insert('Harry')
-        self.assertEqual(first_table.rows, ['Tom', 'Dick', 'Harry'])
+        first_table.insert(['Dick'])
+        first_table.insert(['Harry'])
+        self.assertEqual(first_table.rows, [['Tom'], ['Dick'], ['Harry']])
 
         first_table.commit()
-        self.assertEqual(first_table.rows, ['Tom', 'Dick', 'Harry'])
+        self.assertEqual(first_table.rows, [['Tom'], ['Dick'], ['Harry']])
 
-        first_table.insert('Barry')
-        self.assertEqual(first_table.rows, ['Tom', 'Dick', 'Harry', 'Barry'])
+        first_table.insert(['Barry'])
+        self.assertEqual(first_table.rows, [['Tom'], ['Dick'], ['Harry'], ['Barry']])
 
         first_table.commit()
         second_table = SimpleTable(filename)
-        self.assertEqual(second_table.rows, ['Tom', 'Dick', 'Harry', 'Barry'])
+        self.assertEqual(second_table.rows, [['Tom'], ['Dick'], ['Harry'], ['Barry']])
 
-        second_table.insert('Larry')
-        self.assertEqual(second_table.rows, ['Tom', 'Dick', 'Harry', 'Barry', 'Larry'])
+        second_table.insert(['Larry'])
+        self.assertEqual(second_table.rows, [['Tom'], ['Dick'], ['Harry'], ['Barry'], ['Larry']])
 
         second_table.commit()
         third_table = SimpleTable(filename)
-        self.assertEqual(third_table.rows, ['Tom', 'Dick', 'Harry', 'Barry', 'Larry'])
+        self.assertEqual(third_table.rows, [['Tom'], ['Dick'], ['Harry'], ['Barry'], ['Larry']])
+
+    def test_complex_rows(self):
+        """Test rows with many columns of many different types."""
+        filename = 'table_scratch_complex.json'
+
+        # At start of test, ensure the file doesn't exist.
+        if path.exists(filename):
+            remove(filename)
+
+        first_table = SimpleTable(filename)
+
+        row1 = [123, 'Some text', 'more text', 1.234]      # List containing a mix of types.
+        first_table.insert(row1)
+        row2 = [456, 'other text', 'yet more', 1.678]
+        first_table.insert(row2)
+        first_table.commit()
+
+        second_table = SimpleTable(filename)
+        self.assertEqual(row1, second_table.rows[0])
+        self.assertEqual(row2, second_table.rows[1])
 
 
 if __name__ == '__main__':
