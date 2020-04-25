5984587abcba1905412fd877aea102da7fac943c
johntelforduk <jtelford1@gmail.com>
2020-04-25 19:25:22 +0100

    Fix bug in GrammarBot logic.

--- grammar_bot.py
+++ grammar_bot.py
@@ -1,33 +1,45 @@
 # The grammar robot's logic.
 
+from string import punctuation
+
 
 class GrammarBot:
 
     def __init__(self):
 
-        # The robot should never tweet to the same person twice. This object maintains a suppression list of
-        # users that the bot has sent tweets to in history.
-
         # Pairs of words that the bot will suggest are swapped for each other.
         one_way_swaps = [('accept', 'except'),
+                         ('accepts', 'excepts'),
                          ('affect', 'effect'),
+                         ('affects', 'effects'),
                          ('allusion', 'illusion'),
+                         ('allusions', 'illusions'),
                          ('ascent', 'assent'),
+                         ('ascents', 'assents'),
                          ('breath', 'breathe'),
+                         ('breaths', 'breathes'),
                          ('capital', 'capitol'),
                          ('cite', 'sight'),
+                         ('cites', 'sights'),
                          ('compliment', 'complement'),
+                         ('compliments', 'complements'),
                          ('conscience', 'conscious'),
                          ('council', 'counsel'),
+                         ('councils', 'counsels'),
                          ('elicit', 'illicit'),
                          ('immanent', 'imminent'),
                          ('lead', 'led'),
                          ('lie', 'lay'),
+                         ('lies', 'lays'),
                          ('lose', 'loose'),
+                         ('loses', 'looses'),
                          ('passed', 'past'),
                          ('precede', 'proceed'),
+                         ('precedes', 'proceeds'),
                          ('principal', 'principle'),
+                         ('principals', 'principles'),
                          ('quote', 'quotation'),
+                         ('quotes', 'quotations'),
                          ('reluctant', 'reticent'),
                          ('stationary', 'stationery'),
                          ('than', 'then'),
@@ -39,23 +51,25 @@ class GrammarBot:
 
         # For each one way swap, work out its opposite. And make list of keywords to look for.
         self.two_way_swaps = []
-        self.keywords = []
         for (first, second) in one_way_swaps:
             self.two_way_swaps.append((first, second))
             self.two_way_swaps.append((second, first))
-            self.keywords.append(first)
-            self.keywords.append(second)
 
     def grammar_check(self, message: str) -> str:
         """Returns a suggested substitution if the parm message contains a grammatical error,
         otherwise returns and empty string."""
+
+        # Turn the message into a list of words, all lower case, with all punctuation marks removed.
+        for each_punctuation_mark in punctuation:
+            message = message.replace(each_punctuation_mark, ' ')
+        word_list = message.lower().split()
 
         # Skip tweets that have none of the key words in them. Some replies seem to have neither.
         # Also skip tweets that have more than 1 keyword in them. They might be discussing the grammar difficulty!
         keyword_count = 0
         suggestion = ''
         for (keyword, substitute) in self.two_way_swaps:
-            if keyword in message.lower():                  # Normalise tweets to lower case.
+            if keyword in word_list:
                 keyword_count += 1
                 suggestion = substitute
 
--- test_grammar_bot.py
+++ test_grammar_bot.py
@@ -10,11 +10,14 @@ class TestGrammarBot(unittest.TestCase):
         this_bot = GrammarBot()
 
         self.assertEqual(this_bot.grammar_check('I will take that as a compliment.'), 'complement')
-        self.assertEqual(this_bot.grammar_check('Dave complements our team.'), 'compliment')
+        self.assertEqual(this_bot.grammar_check('Dave complements our team.'), 'compliments')
         self.assertEqual(this_bot.grammar_check('No keywords here.'), '')
         self.assertEqual(this_bot.grammar_check('I find complement and compliment confusing.'), '')
-        self.assertEqual(this_bot.grammar_check('Compliment, compliments, compliments!'), 'complement')
+        self.assertEqual(this_bot.grammar_check('Compliment, compliments, compliments!'), '')
         self.assertEqual(this_bot.grammar_check('Compliment'), 'complement')
+        self.assertEqual(this_bot.grammar_check('Will they be called Coronials?'), '')
+        self.assertEqual(this_bot.grammar_check('Compliment?'), 'complement')
+        self.assertEqual(this_bot.grammar_check('house,stationary'), 'stationery')
 
 
 if __name__ == '__main__':
